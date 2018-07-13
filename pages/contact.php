2f8d5778131189baf18371f171e97d898f71c966
leonard <ballok.leonard@yahoo.com>
2018-07-13 22:31:59 +0100

    interactivity added with jquery and jquery plugins, seo optimization done, html verified for errors, rearranged homepage, version 1.0 finished

--- pages/contact.php
+++ pages/contact.php
@@ -1,8 +1,24 @@
 <!doctype html>
-<html lang="en">
+<html lang="ro">
     <head>
+        <!-- Title -->
+        <title>Contact | Radicalhair.ro</title>
+        <link rel="icon" href="../src/img/icons/faviconRH.ico" type="image/icon">
+        <!-- SEO tags -->
+        <meta name="description" content="Afla datele de contact pentru radicalhair.ro si vezi cum poti lua legatura cu noi. Vezi care sunt datele de contact pentru radicalhair.ro si cum poti intra in legatura cu echipa.">
+        <meta property="fb:app_id" content="194832381205668">
+        <meta property="article:publisher" content="https://www.facebook.com/otiungur/">
+        <meta property="article:author" content="Otilia Ungur">
+        <meta property="og:locale" content="ro_RO">
+        <meta property="og:site_name" content="RadicalHair">
+        <meta property="og:title" content="Contact | radicalhair.ro">
+        <meta property="og:description" content="Afla datele de contact pentru radicalhair.ro si vezi cum poti lua legatura cu noi. Vezi care sunt datele de contact pentru radicalhair.ro si cum poti intra in legatura cu echipa.">
+        <meta name="cXenseParse:pageclass" content="frontpage">
+        <meta name="cXenseParse:url" content="https://www.radicalhair.ro/contact.php">
+
         <!-- Required meta tags -->
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Reset -->
@@ -14,12 +30,12 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+        <!-- Animate CSS -->
+        <link rel="stylesheet" href="../src/css/animate.css">
+
         <!-- Custom stylesheet -->
         <link rel="stylesheet" href="../src/css/style.css">
 
-        <!-- Title -->
-        <title>RadicalHair</title>
-    </head>
     <body>     
         
         <!-- Load Facebook SDK for JavaScript -->
@@ -40,52 +56,52 @@
         logged_out_greeting="Va multumim pentru vizita!">
         </div>
         
-        <!-- Header section -->
-        <header class="sticky-top"> 
-            <nav class="navbar navbar-expand-md navbar-dark">
-                <a class="navbar-brand" href="#"><span class="radical">RADICAL</span><span class="hair">HAIR</span></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                    <ul class="navbar-nav float-right">
-                        <li class="nav-item">
-                            <a class="nav-link" href="../index.html">Acasa</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#presentation">Salon</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#team">Echipa</a>
-                        </li>   
-                        <li class="nav-item">
-                            <a class="nav-link" href="#services">Servicii</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./booking.html">Rezervari</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./termeni.html">T &amp; C</a>
-                        </li> 
-                        <li class="nav-item">
-                            <a class="nav-link" href="./contact.php">Contact</a>
-                        </li>  
-                    </ul>
-                </div>
-            </nav>
-        </header> 
+        <!-- Header section --> 
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <a class="navbar-brand" href="../index.html"><span class="radical">RADICAL</span><span class="hair">HAIR</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav float-right">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.html">Acasa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.html#slider-presentation">Salon</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.html#meet-the-team">Echipa</a>
+                    </li>   
+                    <li class="nav-item">
+                        <a class="nav-link" href="#services">Servicii</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./booking.html">Rezervari</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./termeni.html">T &amp; C</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a class="nav-link active" href="./contact.php">Contact</a>
+                    </li>  
+                </ul>
+            </div>
+        </nav>
 
         <section id="contact">
             <div class="container">
-                <div class="row">  
+                <div class="row js--wp-1">  
                     <div class="contact-split">
-                        <div class="contact-wrap">
-                            <h1>Detalii contact</h1>
+                        <div class="contact-wrap js--nav-scroll">
+                            <h1 class="animatedslow infinite pulse">Detalii contact</h1>
                             <h2>RADICAL HAIR SRL</h2>
                             <p>Adresa: Str. Ion ionescu de la brad<br>Timisoara, Judetul Timis</p>
                             <br>
-                            <p>Telefon: +40(788) 432 444</p>
+                            <p>TELEFON:  +40 (744) 156 384</p>
                             <p>office@radicalhair.ro</p>
+                            <br>
+                            <p><a href="https://www.facebook.com/otiungur/">viziteaza-ne pagina de facebook aici</a><p>
                             <br>
                             <h2>Pogram:</h2>
                             <p>L-V: 09:00 - 18:00</p>
@@ -122,27 +138,12 @@
                                     <input type="email" name="email" id="email" placeholder="Adresa de Email" required>
                                 </div>
                                 <div class="form-row">
-                                    <label for="find-us">Cum ati auzit de noi?</label>
-                                    <select>
-                                        <option value="prieteni" selected>Prieteni</option>
-                                        <option value="google">Google</option>
-                                        <option value="anunt">Reclama</option>
-                                        <option value="altele">Alte surse</option>
-                                    </select>
-                                </div>
-                                <div class="form-row">
-                                    <label for="news">
-                                        <span class="checkbox">Doresc sa primesc Newsletter</span>
-                                        <input type="checkbox" name="news" id="news">
-                                    </label>  
-                                </div>
-                                <div class="form-row">
                                     <label><span class="red">* </span> Mesajul tau</label>
                                     <textarea name="message" id="message"></textarea>
                                 </div>
                                 <div class="form-row">
                                     <label>&nbsp;</label>
-                                    <input type="submit" value="Trimite">
+                                    <button type="submit" class="btn btn-custom">Trimite</button>
                                 </div>
                             </form>
                         </div>
@@ -156,47 +157,62 @@
         <footer>
             <div class="container">
                 <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                    <iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJcznwW7tnRUcRYM_PcCeWolI&key=AIzaSyC_HHMaB8iF1LSnMUPs3ogB1ZT_Vrapdyw" allowfullscreen></iframe>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-
-                    <div>
-                        <h2>Contact:</h2>
-                        <p>TELEFON:  +40 (744) 156 384</p>
-                        <P>ADRESA: Strada Dr. Grigore T. Popa Nr 15, Timisoara</P>
-                        <p>Salon Radical Hair</p>
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 map">
+                        <iframe id="map" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJcznwW7tnRUcRYM_PcCeWolI&key=AIzaSyC_HHMaB8iF1LSnMUPs3ogB1ZT_Vrapdyw" allowfullscreen></iframe>
                     </div>
-                    <div>
-                        <h2>Orar</h2>
-                        <table class="table table-bordered table-striped">
-                            <tbody>
-                            <tr>
-                                <td>Luni - Vineri</td>
-                                <td>09:00 - 18:00</td>
-                            </tr>
-                                <td>Sambata</td>
-                                <td>09:00 - 12:00</td>
-                            </tr>
-                            <tr>
-                                <td>Duminica</td>
-                                <td><span class="red">Inchis</span></td>
-                            </tr>
-                            </tbody>
-                        </table>
-                        <p>&copy Copyright 2018 SC RADICALHAIR SRL</p>
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div>
+                            <h2 class="animatedslow infinite pulse">Contact:</h2>
+                            <p>TELEFON:  +40 (744) 156 384</p>
+                            <P>ADRESA: Strada Dr. Grigore T. Popa Nr 15, Timisoara</P>
+                            <p>Salon Radical Hair</p>
+                        </div>
+                        <div>
+                            <h2>Orar</h2>
+                            <table class="table table-bordered table-striped">
+                                <tbody>
+                                    <tr>
+                                        <td>Luni - Vineri</td>
+                                        <td>09:00 - 18:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Sambata</td>
+                                        <td>09:00 - 12:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Duminica</td>
+                                        <td><span class="red">Inchis</span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <p>&copy; Copyright 2018 SC RADICALHAIR SRL</p>
+                        </div>
                     </div>
-
-                </div>
                 </div>
             </div>
         </footer>
 
-        <!-- JavaScript -->
-        <script type="text/javascript" src="../src/javascript/index.js"></script>
+
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <!-- Jquery plugins -->
+          <!-- waypoint for scrolling effects -->
+          <script type="text/javascript" src="../src/javascript/jquery.waypoints.min.js"></script>
+        <!-- JavaScript -->
+        <script type="text/javascript" src="../src/javascript/index.js"></script>
+        
+        <!-- Google analytics -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122325580-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-122325580-1');
+        </script>
+
     </body>
 </html>
---
+++ src/javascript/index.js
@@ -0,0 +1,90 @@
+/************************************************************
+ * Jquery
+ ************************************************************/
+
+ $(document[0]).ready(function() {
+
+    /* Sticky navigation */
+    
+     $('.js--nav-scroll').waypoint(function(direction) {
+         if(direction == "down") {
+            $('nav').addClass('fixed-top animated fadeInDown');
+         } else {
+            $('nav').addClass('fadeOutUp');
+            setTimeout(function() {
+                $('nav').removeClass('fixed-top animated fadeOutUp fadeInDown');
+            }, 500);
+         }
+     }, {
+     offset: '15%;'
+     });
+
+     /* Scroll effect */
+     
+     $('.js--wp-1').waypoint(function(direction){
+         $('.js--wp-1').addClass('animated fadeIn')
+     }, {
+         offset: '80%'
+     });
+
+     $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-5').waypoint(function(direction){
+        $('.js--wp-5').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-6').waypoint(function(direction){
+        $('.js--wp-6').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-7').waypoint(function(direction){
+        $('.js--wp-7').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-8').waypoint(function(direction){
+        $('.js--wp-8').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-9').waypoint(function(direction){
+        $('.js--wp-9').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-10').waypoint(function(direction){
+        $('.js--wp-10').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-11').waypoint(function(direction){
+        $('.js--wp-11').addClass('animated fadeIn')
+    }, {
+        offset: '80%'
+    });
+
+ })
